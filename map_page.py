ad932705357fecc4de76b691c50efc0e8cd3bbe6
Max Omdal <mjomdal@gmail.com>
2020-01-26 09:47:17 -0600

    updates

--- map_page.py
+++ map_page.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect
 import database
+import json
 app = Flask(__name__)
 
 @app.route('/')
@@ -23,6 +24,19 @@ def publish_post():
         lat = data.pop("lat")
         lng = data.pop("lng")
         database.addPost(data, lat, lng)
-        print(data)
         print("created new entry in database")
     return redirect('/')
+
+@app.route('/favorited/<postID>')
+def favorite_post(postID):
+    if request.method == 'GET':
+        '''favorite the post with id'''
+        database.incrementFavorites(postID)
+    return redirect('/')
+
+@app.route('/feed/<latitude>/<longitude>')
+def show_feed(latitude, longitude):
+    posts = database.getPosts(float(longitude), float(latitude))
+    posts = json.loads(posts)
+    print(json.dumps(posts, indent=4, sort_keys=True))
+    return render_template('feed.html', posts=posts["posts"])
