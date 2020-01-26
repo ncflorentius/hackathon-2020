09a14100919d8bd79db38e9d1ab9661a3033cae7
Max Omdal <mjomdal@gmail.com>
2020-01-26 02:02:54 -0600

    add post page

--- map_page.py
+++ map_page.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 import database
 app = Flask(__name__)
 
@@ -9,3 +9,15 @@ def show_map():
 @app.route('/<latitude>/<longitude>')
 def get_posts_near_loc(latitude, longitude):
     return database.getPosts(float(longitude), float(latitude))
+
+@app.route('/create')
+def create_post():
+    return render_template('addPage.html')
+
+@app.route('/submit', methods=['POST'])
+def publish_post():
+    if request.method == 'POST':
+        '''create new post'''
+        data = request.form # a multidict containing POST data
+        database.addPost(data)
+    return redirect('/')
